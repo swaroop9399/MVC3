c8babab4e28411e6d476987142d4ba8ab4451687
Swaroop Sanjay Patil <swaroop@apl.com>
2022-08-27 15:14:54 +0530

    MVC3

---
+++ MyProject3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MyProject3.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace MyProject3.Controllers
+{
+    public class HomeController : Controller
+    {
+        string conString = "Data Source = LTP_RD_411; Initial Catalog = test; Integrated Security = True;";
+        public IActionResult AllData()
+        {
+            string query = "select * from Users3";
+            SqlConnection con = new SqlConnection(conString);
+            var list = con.Query<Student>(query).ToList();
+            
+            return View(list);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Student s)
+        {
+            if (s.isChecked == "on")
+            {
+                string Insert = $"Insert into Users3(Name, Email, Password, Gender, Course) values('{s.Name}', '{s.Email}', '{s.Password}', '{s.Gender}', '{s.Course}')";
+                IDbConnection con = new SqlConnection(conString);
+                con.Execute(Insert);
+                return new JsonResult("Inserted");
+            }
+            else
+            {
+                Console.WriteLine("Not working");
+                return new JsonResult("Failed");
+            }
+            
+        }
+        
+    }
+}
---
+++ MyProject3/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace MyProject3.Models
+{
+    public class Student
+    {
+        public int UID { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Course { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+        public string isChecked { get; set; }
+    }
+
+
+
+}
